85e0d4b6e545855b6d21a2d54c55465e432f9663
muzisanshi <1052048489@qq.com>
2019-11-27 14:36:28 +0800

    优化爬虫

--- modules/hacker/crawler.js
+++ modules/hacker/crawler.js
@@ -1,42 +1,166 @@
 
 let Crawler = require('crawler');
 let fs = require('fs');
-let cl = new Crawler({
-	callback:(err,res,done)=>{
-		let imgs = res.$("img");
-		let imgSrcs = [];
-		let as = res.$("a");
-		let uris = [];
-		let saveRoot = "./website/image/jd/";
 
-		imgs.map((index,item)=>{
-			// 下载图片
-			let uri = res.$(item).attr("src");
-			let imgName = uri.split("/");
-			imgName = imgName[imgName.length - 1];
-
-			if(uri.indexOf("http")==-1&&uri.indexOf("//")>-1){
-				uri = "http:"+uri;
-			}
-
-			uris.push({
-				uri:uri,
-				encoding:null,
-				jQuery:false,
-				callback:(e,r,d)=>{
-					// 先创建文件
-					fs.writeFile(saveRoot+imgName,"null",'utf8',function(err){
-						fs.createWriteStream(saveRoot+imgName).write(r.body);
-						d();
-					});
-				}
-			});
-			imgSrcs.push(uri);
-		});
-		fs.writeFile(saveRoot+"urls.json",JSON.stringify(imgSrcs),'utf8',function(err){});
-		cl.queue(uris);
-		done();
+class LCrawler{
+	
+	constructor(){
+		this.cl = new Crawler();
 	}
-});
-let target = "https://www.youku.com/";
-cl.queue(target);
+
+	checkImgName(type,name){
+
+		let inCopy = name;
+		// 判断图片名称是否有后缀
+		if(!(inCopy.indexOf(".jpg")>-1) && !(inCopy.indexOf(".png")>-1) && !(inCopy.indexOf(".gif")>-1) && !(inCopy.indexOf(".svg")>-1) 
+			&& !(inCopy.indexOf(".tif")>-1) && !(inCopy.indexOf(".bmp")>-1)){
+			// 判断返回图片格式
+			switch(type){
+				case "image/jpeg":
+					name = `${name}.jpg`;
+					break;
+				case "image/png":
+					name = `${name}.png`;
+					break;
+				case "image/gif":
+					name = `${name}.gif`;
+					break;
+				case "image/svg+xml":
+					name = `${name}.svg`;
+					break;
+				case "image/tiff":
+					name = `${name}.tif`;
+					break;
+				case "image/bmp":
+					name = `${name}.bmp`;
+					break;
+			}
+		}
+		return name;
+	}
+
+	async crawPage(url){
+
+		if(url){
+
+			return await new Promise((resolve,reject)=>{
+
+				this.cl.queue([{
+					uri:url,
+					callback:(e,r,d)=>{
+						if(e){
+							reject(`CrawPageError_${url}:${e}`);
+						}
+						if(r){
+							resolve(r);
+						}
+						d();
+					}
+				}]);
+
+			});
+		}
+	}
+
+	/*
+	 * 根据图片url抓去图片
+	 */
+	async crawSingleImg(url,savePath){
+
+		if(url){
+
+			return await new Promise((resolve,reject)=>{
+				this.cl.queue([{
+					uri:url,
+					encoding:null,
+					jQuery:false,
+					callback:(e,r,d)=>{
+						if(e){
+							reject(`CrawPageError_${url}:${e}`);
+						}
+						if(r){
+
+							let imgName = url.split("?")[0].split("/");
+							imgName = imgName[imgName.length - 1];
+							imgName = this.checkImgName(r.headers['content-type'],imgName);
+
+							// 保存图片
+							let sp = `${savePath}${imgName}`;
+							fs.writeFile(sp,"null",'utf8',function(err){
+								fs.createWriteStream(sp).write(r.body);
+								d();
+							});
+						}
+						d();
+					}
+				}]);
+			})
+
+		}
+	}
+
+	/*
+	 * 从网页抓去所有图片
+	 */
+	async crawImgs(url,savePath){
+
+		if(url){
+
+			return await new Promise((resolve,reject)=>{
+				// 先获取页面
+				this.crawPage(url).then((res)=>{
+
+					let imgs = res.$("img");
+					let imgSrcs = [];
+					let uris = [];
+
+					imgs.map((index,item)=>{
+
+						// 下载图片
+						let uri = res.$(item).attr("src");
+						if(uri.indexOf("http")==-1&&uri.indexOf("//")>-1){
+							uri = "http:"+uri;
+						}
+
+						uris.push({
+							uri:uri,
+							encoding:null,
+							jQuery:false,
+							callback:(e,r,d)=>{
+								if(e){
+									console.error(`DownloadImgError_${uri}:${r}`);
+								}
+								if(r){
+
+									let imgName = uri.split("?")[0].split("/");
+									imgName = imgName[imgName.length - 1];
+									imgName = this.checkImgName(r.headers['content-type'],imgName);
+
+									// 保存图片
+									let sp = `${savePath}${imgName}`;
+									fs.writeFile(sp,"null",'utf8',function(err){
+										fs.createWriteStream(sp).write(r.body);
+										d();
+									});
+								}
+							}
+						});
+						imgSrcs.push(uri);
+					});
+					console.table(imgSrcs);
+					this.cl.queue(uris);
+
+				}).catch((err)=>{
+					reject(err);
+				})
+			});
+
+		}
+	}
+
+
+}
+
+const instance = new LCrawler();
+module.exports = instance;
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let koa = require('koa');
 let koaStatic = require('koa-static');
 let fs = require('fs');
 let path = require('path');
+let crawler = require('./modules/hacker/crawler');
 let app = new koa();
 
 app.use(koaStatic(__dirname+'/www/website'));
@@ -15,6 +16,12 @@ app.listen(8080,()=>{
 	console.log("server ok");
 	
 });
+
+let url = "http://www.youku.com";
+// let url = "https://ykimg.alicdn.com/develop/image/2019-11-21/b90fa5a923d8093759b23d7aeead52f5.png";
+// let url = "http://r1.ykimg.com/050C00005CC2941B859B5E3AFC07E8F6?x-oss-process=image/resize,w_290/interlace,1/quality,Q_80/sharpen,100"
+crawler.crawImgs(url,`${__dirname}/www/static/`);
+// crawler.crawSingleImg(url,`${__dirname}/www/static/`);
 
 
 
